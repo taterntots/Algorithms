9720a03935a3d33d532bd08e00358b6fb4e30b0d
taterntots <matthew.a.weidner@gmail.com>
2020-04-16 18:08:29 -0400

    working on rock paper scissors. very difficult. not solved

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,8 +3,40 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
+ 
+ # define list of all possible plays and RPS variables
+  rps = ['rock', 'paper', 'scissors']
+  # possible_plays = []
 
+  # If no players, game does not start
+  if n == 0:
+    return [[]]
+  # If one player, there are only three possibilities
+  elif n == 1:
+    return [rps]
+  # Logic to figure out all possible plays
+  else:
+    base = rock_paper_scissors(n-1)
+    print(f'BASE: ', base)
+    possible_plays = []
+    for each in base:
+      
+      # print(f'EACH:', each)
+
+      possible_plays += [each + ['rock'], each + ['paper'], each + ['scissors']]
+      # possible_plays.append(each + ['rock'])
+      # possible_plays.append(each + ['paper'])
+      # possible_plays.append(each + ['scissors'])
+      # print(f'PPLAYS', possible_plays)
+
+    # for p1 in range(len(rps)):
+    #   for p2 in range(len(rps)):
+    #     print(rps[p1], rps[p2])
+    #     possible_plays.append(rps[p1])
+  
+  return possible_plays
+
+print(f'POSSIBLE PLAYS: ', rock_paper_scissors(2))
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
