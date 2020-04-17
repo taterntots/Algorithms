68b06acdec58070432652548b5ea4c9a05f9854c
taterntots <matthew.a.weidner@gmail.com>
2020-04-16 20:23:20 -0400

    rock paper scissors is solved!

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,38 +2,65 @@
 
 import sys
 
-def rock_paper_scissors(n):
+# def rock_paper_scissors(n):
  
- # define list of all possible plays and RPS variables
-  rps = ['rock', 'paper', 'scissors']
-  # possible_plays = []
+#  # define list of all possible plays and RPS variables
+#   rps = ['rock', 'paper', 'scissors']
+#   # possible_plays = []
+
+#   # If no players, game does not start
+#   if n == 0:
+#     return [[]]
+#   # If one player, there are only three possibilities
+#   elif n == 1:
+#     return [rps]
+#   # Logic to figure out all possible plays
+#   else:
+#     base = rock_paper_scissors(n-1)
+#     print(f'BASE: ', base)
+#     possible_plays = []
+#     for each in base:
+#       for j in rps:
+#         new_each = each.copy()
+#         new_each.append(j)
+#         possible_plays.append(new_each)
+#       # print(f'EACH:', each)
+
+#         # possible_plays += [each + ['rock'], each + ['paper'], each + ['scissors']]
+#       # possible_plays.append(each + ['rock'])
+#       # possible_plays.append(each + ['paper'])
+#       # possible_plays.append(each + ['scissors'])
+#       # print(f'PPLAYS', possible_plays)
+
+#     # for p1 in range(len(rps)):
+#     #   for p2 in range(len(rps)):
+#     #     print(rps[p1], rps[p2])
+#     #     possible_plays.append(rps[p1])
+  
+#   return possible_plays
+
+def rock_paper_scissors(n):
+
+  # define list of all possible plays and RPS variables
+  rps = [['rock'], ['paper'], ['scissors']]
+  possible_plays = []
 
   # If no players, game does not start
   if n == 0:
     return [[]]
   # If one player, there are only three possibilities
   elif n == 1:
-    return [rps]
+    return rps
+
   # Logic to figure out all possible plays
   else:
     base = rock_paper_scissors(n-1)
-    print(f'BASE: ', base)
-    possible_plays = []
-    for each in base:
-      
-      # print(f'EACH:', each)
+    for x in base:
+      print(f'HOPE THIS WORKS', x + ['rock'], x + ['paper'])
+      possible_plays.append(x + ['rock'])
+      possible_plays.append(x + ['paper'])
+      possible_plays.append(x + ['scissors'])
 
-      possible_plays += [each + ['rock'], each + ['paper'], each + ['scissors']]
-      # possible_plays.append(each + ['rock'])
-      # possible_plays.append(each + ['paper'])
-      # possible_plays.append(each + ['scissors'])
-      # print(f'PPLAYS', possible_plays)
-
-    # for p1 in range(len(rps)):
-    #   for p2 in range(len(rps)):
-    #     print(rps[p1], rps[p2])
-    #     possible_plays.append(rps[p1])
-  
   return possible_plays
 
 print(f'POSSIBLE PLAYS: ', rock_paper_scissors(2))
